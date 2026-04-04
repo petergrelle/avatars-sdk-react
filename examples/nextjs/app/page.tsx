e4fcaed434cc46cea695b56c04f4b1e40a7763cb
petergrelle <petercgrelle@gmail.com>
2026-04-04 12:40:38 -0600

    Update avatarId in AvatarCall component

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main style={{ width: '100vw', height: '100vh', backgroundColor: '#000' }}>
       <AvatarCall 
-        avatarId="083e35dc-a076-479d-b724-96aa8462c429" 
+        avatarId="eaa8b03d-0a6a-4bd0-83a9-039609b47808" 
         connectUrl="/api/avatar/connect"
       />
     </main>
